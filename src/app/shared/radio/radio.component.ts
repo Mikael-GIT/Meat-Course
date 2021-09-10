1a2d289062a084fd3ecb243f06545b9e4792cfd8
Mikael Tavares <mikael.tavares@unigranrio.br>
2021-09-10 01:32:25 -0300

    Adicionando ControlValueAccessor.

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,11 +1,18 @@
 import { RadioOption } from './RadioOption';
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
   constructor() { }
 
@@ -13,11 +20,25 @@ export class RadioComponent implements OnInit {
 
   value: any
 
+  onChange: any
+
   ngOnInit() {
   }
 
   setValue(value: any) {
     this.value = value;
+    this.onChange(this.value)
   }
 
+  writeValue(obj: any): void { //Método que vai ser chamado pelas diretivas quando elas quiserem passar um valor para este componente
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void { //As diretivas passam uma função pra gente que tem que ser chamada sempre que o valor interno do componente mudar
+    this.onChange = fn
+  }
+
+  registerOnTouched(){
+
+  }
 }
