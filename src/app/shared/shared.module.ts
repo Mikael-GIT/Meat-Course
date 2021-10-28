5108dae4b5d01733cbe884a83c53f9a473f0804e
Mikael Tavares <mikael.tavares@unigranrio.br>
2021-10-27 22:23:33 -0300

    Revert "ajuste"
    This reverts commit e64a57638ab9b7e195cdcf1de5ddefab2d2498af.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,9 @@
-import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shopping-cart.service';
-import { RestaurantService } from './../restaurants/restaurants.service';
-import { OrderService } from './../order/order.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RatingComponent } from './rating/rating.component';
 import { RadioComponent } from './radio/radio.component';
 import { InputComponent } from './input/input.component';
-import {NgModule, ModuleWithProviders} from '@angular/core';
+import {NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
@@ -25,12 +22,7 @@ import { RouterModule, Routes } from '@angular/router';
     ReactiveFormsModule,
     CommonModule
   ]
-})
+}, )
 export class SharedModule {
-  static forRoot(): ModuleWithProviders{
-    return {
-      ngModule: SharedModule,
-      providers: [ShoppingCartService, RestaurantService, OrderService]
-    }
-  }
+
 }
