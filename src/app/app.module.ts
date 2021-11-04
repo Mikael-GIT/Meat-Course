4c0189f97ea1082b0b55ad58029506160200ff2c
Mikael Tavares <mikael.tavares@unigranrio.br>
2021-11-04 00:41:30 -0300

    Adicionando componente no header para realizar login e utilizando if else para fazer controle de exibicao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './security/login/login.component';
+import { UserDetailComponent } from './header/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { LoginComponent } from './security/login/login.component';
     OrderSummaryComponent,
     NotFoundComponent,
     LoginComponent,
+    UserDetailComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -1,4 +1,4 @@
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
@@ -6,13 +6,17 @@ import { HttpClient } from '@angular/common/http';
 import { MEAT_API } from 'app/app.api';
 import 'rxjs/add/operator/do'
 import { User } from './user.model';
-
+import 'rxjs/add/operator/filter'
 @Injectable()
 export class LoginService {
 
   user: User
+  lastUrl: string
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {
+    this.router.events.filter(e => e instanceof NavigationEnd)
+                      .subscribe((e: NavigationEnd) => this.lastUrl = e.url);
+  }
 
   isLoggedIn(): boolean {
     return this.user !== undefined
@@ -23,7 +27,11 @@ export class LoginService {
     .do(user => this.user = user)
   }
 
-  handleLogin(path?: string){
+  handleLogin(path: string = this.lastUrl){
     this.router.navigate(['/login', btoa(path)])
+  }
+
+  logout(){
+    this.user = undefined;
   }
 }
